8421b93556b58c5eaa35f2fe8a3b8b4709856e64
JSnakegitHub <nyekowalter69@gmail.com>
2019-08-15 14:15:05 +0300

    fetch pricings

--- src/pages/terms.jsx
+++ src/pages/terms.jsx
@@ -29,19 +29,64 @@ const Terms = () => {
           </div>
           <div className="row">
             <div className="col-12 col-md-8">
+              <h3>OVERVIEW</h3>
               <p className="font-size-lg text-gray-800 mb-6 mb-md-8">
-                Below are our Terms of Service, which outline a lot of legal
-                goodies, but the bottom line is it’s our aim to always take care
-                of both you, as a customer, or as a seller on our platform.
+                {" "}
+                This website is operated by BatchPack. Throughout the site, the
+                terms “we”, “us” and “our” refer to BatchPack. BatchPack offers
+                this website, including all information, tools and services
+                available from this site to you, the user, conditioned upon your
+                acceptance of all terms, conditions, policies and notices stated
+                here.
               </p>
+              <br />
+              <p>
+                By visiting our site and/ or purchasing something from us, you
+                engage in our “Service” and agree to be bound by the following
+                terms and conditions (“Terms of Service”, “Terms”), including
+                those additional terms and conditions and policies referenced
+                herein and/or available by hyperlink. These Terms of Service
+                apply to all users of the site, including without limitation
+                users who are browsers, vendors, customers, merchants, and/ or
+                contributors of content.
+              </p>
+              <br />
+              <p>
+                Please read these Terms of Service carefully before accessing or
+                using our website. By accessing or using any part of the site,
+                you agree to be bound by these Terms of Service. If you do not
+                agree to all the terms and conditions of this agreement, then
+                you may not access the website or use any services. If these
+                Terms of Service are considered an offer, acceptance is
+                expressly limited to these Terms of Service.
+              </p>
+              <br />
+              <p>
+                Any new features or tools which are added to the current store
+                shall also be subject to the Terms of Service. You can review
+                the most current version of the Terms of Service at any time on
+                this page. We reserve the right to update, change or replace any
+                part of these Terms of Service by posting updates and/or changes
+                to our website. It is your responsibility to check this page
+                periodically for changes. Your continued use of or access to the
+                website following the posting of any changes constitutes
+                acceptance of those changes.
+              </p>
+              <br />
+              <p>
+                Our store is hosted on Shopify Inc. They provide us with the
+                online e-commerce platform that allows us to sell our products
+                and services to you.
+              </p>
+              <br />
 
               <h3 className="mb-5">Licensing Terms</h3>
 
               <p className="text-gray-800">
-                By visiting and/or taking any action on BatchPack, you
-                confirm that you are in agreement with and bound by the terms
-                outlined below. These terms apply to the website, emails, or any
-                other communication.
+                By visiting and/or taking any action on BatchPack, you confirm
+                that you are in agreement with and bound by the terms outlined
+                below. These terms apply to the website, emails, or any other
+                communication.
               </p>
 
               <p>Here are terms of our Standard License:</p>
@@ -123,10 +168,10 @@ const Terms = () => {
               <h3 className="mb-5">Additional Terms</h3>
 
               <p className="text-gray-800">
-                By visiting and/or taking any action on BatchPack, you
-                confirm that you are in agreement with and bound by the terms
-                outlined below. These terms apply to the website, emails, or any
-                other communication.
+                By visiting and/or taking any action on BatchPack, you confirm
+                that you are in agreement with and bound by the terms outlined
+                below. These terms apply to the website, emails, or any other
+                communication.
               </p>
 
               <div className="d-flex">
@@ -148,8 +193,8 @@ const Terms = () => {
                 <p className="text-gray-800">
                   You have 14 days to evaluate your purchase. If your purchase
                   fails to meet expectations set by the seller, or is critically
-                  flawed in some way, contact BatchPack and we will issue
-                  a full refund pending a review.
+                  flawed in some way, contact BatchPack and we will issue a full
+                  refund pending a review.
                 </p>
               </div>
               <div className="d-flex">
